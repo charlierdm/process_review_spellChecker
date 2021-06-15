274d2cd6ee910eaa28c65489ad6b63a378a11e0b
Charlie <charlie.davismiller@gmail.com>
2021-06-15 14:14:41 +0100

    spellChecker feature created, all tests are passing

---
+++ spellChecker.js
@@ -0,0 +1,25 @@
+const spellChecker = (word, dictionary) => {
+  let lower = word.toLowerCase().split(" ");
+
+  let result = [];
+
+  if (lower.length === 0) {
+    throw new Error("No words were found.");
+  }
+  if (lower.join(" ") === dictionary.join(" ")) {
+    return word;
+  }
+  if (lower !== dictionary) {
+    for (let i = 0; i < lower.length; i++) {
+      if (lower[i] === dictionary[i]) {
+        result.push(lower[i]);
+      } else {
+        result.push(`~${lower[i]}~`);
+      }
+    }
+  }
+
+  return result.join(" ");
+};
+
+module.exports = spellChecker;
---
+++ spellChecker.test.js
@@ -0,0 +1,41 @@
+const spellChecker = require("./spellChecker");
+
+describe("spellChecker", () => {
+  it("returns a correctly spelt single word", () => {
+    expect(spellChecker("time", ["time"])).toEqual("time");
+  });
+
+  it("returns an incorrectly spelt word with tildas", () => {
+    expect(spellChecker("tme", ["time"])).toEqual("~tme~");
+  });
+
+  it("returns an incorrectly spelt word with tildas", () => {
+    expect(spellChecker("cta", ["cat"])).toEqual("~cta~");
+  });
+
+  it("returns a capitalized word that is spelt correctly", () => {
+    expect(spellChecker("Hand", ["hand"])).toEqual("Hand");
+  });
+
+  it("returns a capitalized word that is spelt correctly", () => {
+    expect(spellChecker("haNd", ["hand"])).toEqual("haNd");
+  });
+
+  it("returns two words that are all spelt correctly", () => {
+    expect(spellChecker("the clock", ["the", "clock"])).toEqual("the clock");
+  });
+
+  it("returns two words, one is incorrectly spelt with tildas", () => {
+    expect(spellChecker("teh clock", ["the", "clock"])).toEqual("~teh~ clock");
+  });
+
+  it("returns two words, both incorrectly spelt with tildas", () => {
+    expect(spellChecker("teh clokc", ["the", "clock"])).toEqual("~teh~ ~clokc~")
+  })
+
+  it("throws an error if passed an empty string", () => {
+    expect(() => {
+      spellChecker("", ["sandwhich"]).toThrow("No words found.")
+    })
+  });
+});
